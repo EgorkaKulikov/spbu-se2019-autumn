8533eb5e963f8c2bb11e0fbf4a849ae158299da0
azaat <a.r.akhmetyanov@gmail.com>
2019-10-26 04:07:48 +0300

    Code refactor

--- Task01/linear_solve.c
+++ Task01/linear_solve.c
@@ -5,52 +5,55 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_linalg.h>
+#include "malloc_check.h"
 
-void copy_sle(int size
+void copy_sle(size_t size
               , double *matrix[size]
               , double *b
               , double *matrix_copy[size]
               , double *b_copy) {
     for (int i = 0; i < size; i++) {
-        matrix_copy[i] = malloc(size * sizeof(double));
+        matrix_copy[i] = malloc_check(size * sizeof(double));
         memcpy(matrix_copy[i], matrix[i], size * sizeof(double));
     }
     memcpy(b_copy, b, size * sizeof(double));
 }
 
-void generate_vector(int size, double *b) {
+void generate_vector(size_t size, double *b_vector) {
     srand(time(NULL));
     for (int i = 0; i < size; i++) {
-        b[i] = rand() % RAND_MAX + 1;;
+        b_vector[i] = rand() % RAND_MAX + 1;;
     }
 }
 
-void generate_matrix(int size, double *matrix[size]) {
+void generate_matrix(size_t size, double *matrix[size]) {
     srand(time(NULL));
     for (int i = 0; i < size; i++) {
-        matrix[i] = malloc(size * sizeof(double));
+        matrix[i] = malloc_check(size * sizeof(double));
         for (int j = 0; j < size; j++) {
             matrix[i][j] = rand() % RAND_MAX + 1;
         }
     }
 }
 
-void gsl_solve(int size, double *matrix_flat, double *b, double *x) {
+void gsl_solve(size_t size, double *matrix_flat, double *b_vector, double *x_vector) {
     gsl_matrix_view gsl_matrix = gsl_matrix_view_array(matrix_flat, size, size);
-    gsl_vector_view gsl_b = gsl_vector_view_array(b, size);
+    gsl_vector_view gsl_b = gsl_vector_view_array(b_vector, size);
     
+    int signum;
     gsl_vector *gsl_x = gsl_vector_alloc(size);
-
-    gsl_linalg_HH_solve(&gsl_matrix.matrix, &gsl_b.vector, gsl_x);
+    gsl_permutation *gsl_perm = gsl_permutation_alloc(size);
+    gsl_linalg_LU_decomp(&gsl_matrix.matrix, gsl_perm, &signum);
+    gsl_linalg_LU_solve(&gsl_matrix.matrix, gsl_perm, &gsl_b.vector, gsl_x);
 
     for (int i = 0; i < size; i++) {
-        x[i] = gsl_vector_get(gsl_x, i);
+        x_vector[i] = gsl_vector_get(gsl_x, i);
     }
 
     gsl_vector_free(gsl_x);
 }
 
-void sequential_solve(int size, double *matrix[size], double *b, double *x) {
+void sequential_solve(size_t size, double *matrix[size], double *b_vector, double *x_vector) {
     int pivot_row = 0;
     int pivot_col = 0;
     double multiplier, pivot;
@@ -69,7 +72,7 @@ void sequential_solve(int size, double *matrix[size], double *b, double *x) {
             for (int k = pivot_col + 1; k < size; k++) {
                 matrix[j][k] -= multiplier * matrix[pivot_row][k];
             } 
-            b[j] -= multiplier * b[pivot_row];
+            b_vector[j] -= multiplier * b_vector[pivot_row];
         }
 
         pivot_row++;
@@ -79,15 +82,14 @@ void sequential_solve(int size, double *matrix[size], double *b, double *x) {
 
     //backwards
     for (int k = size - 1; k >= 0; k--) {
-        x[k] = b[k];
+        x_vector[k] = b_vector[k];
         for (int i = k + 1; i < size; i++)
-            x[k] -= matrix[k][i] * x[i];
-        x[k] /= matrix[k][k];
+            x_vector[k] -= matrix[k][i] * x_vector[i];
+        x_vector[k] /= matrix[k][k];
     }
 }
 
-void omp_solve(int size, double *matrix[size], double *b, double *x) {
-    omp_set_num_threads(omp_get_num_procs());
+void omp_solve(size_t size, double *matrix[size], double *b_vector, double *x_vector) {
     int pivot_row = 0;
     int pivot_col = 0;
     double multiplier, pivot;
@@ -108,7 +110,7 @@ void omp_solve(int size, double *matrix[size], double *b, double *x) {
             for (int k = pivot_col + 1; k < size; k++) {
                 matrix[j][k] -= multiplier * matrix[pivot_row][k];
             } 
-            b[j] -= multiplier * b[pivot_row];
+            b_vector[j] -= multiplier * b_vector[pivot_row];
         }
 
         pivot_row++;
@@ -118,10 +120,11 @@ void omp_solve(int size, double *matrix[size], double *b, double *x) {
 
     //backwards
     for (int k = size - 1; k >= 0; k--) {
-        x[k] = b[k];
+        x_vector[k] = b_vector[k];
+        #pragma omp simd
         for (int i = k + 1; i < size; i++)
-            x[k] -= matrix[k][i] * x[i];
-        x[k] /= matrix[k][k];
+            x_vector[k] -= matrix[k][i] * x_vector[i];
+        x_vector[k] /= matrix[k][k];
     }
 }
 
--- Task01/linear_solve.h
+++ Task01/linear_solve.h
@@ -1,24 +1,24 @@
-void sequential_solve(int size
+void sequential_solve(size_t size
                       , double *matrix[size]
-                      , double *b
-                      , double *x);
+                      , double *b_vector
+                      , double *x_vector);
 
-void omp_solve(int size
+void omp_solve(size_t size
                     , double *matrix[size]
-                    , double *b
-                    , double *x);
+                    , double *b_vector
+                    , double *x_vector);
 
-void gsl_solve(int size
+void gsl_solve(size_t size
                , double matrix[size * size]
-               , double *b
-               , double *x);
+               , double *b_vector
+               , double *x_vector);
 
-void generate_vector(int size, double *b);
+void generate_vector(size_t size, double *b_vector);
 
-void generate_matrix(int size, double *matrix[size]);
+void generate_matrix(size_t size, double *matrix[size]);
 
 void copy_sle(int size
               , double *matrix[size]
-              , double *b
+              , double *b_vector
               , double *matrix_copy[size]
-              , double *b_copy);
+              , double *b_vector_copy);
