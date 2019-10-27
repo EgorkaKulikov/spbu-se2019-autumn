225b1b7a08b7b524567ef4a3ec9bcc1b5f9afb3a
AlekseiPrivalihin <aleksei.privalihin@gmail.com>
2019-10-27 22:17:48 +0300

    Task03_Privalikhin

---
+++ Task03/Task03/Shared.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task03
+{
+    public static class Shared<T>
+    {
+        public const int MaxInt32 = 1 << 30;
+        public const int MaxSecTimeout = 3;
+        
+        public static readonly Random RandomGenerator = new Random();
+        public static readonly List<T> Buff = new List<T>();
+        
+        public static readonly Semaphore IsEmpty = new Semaphore(0, MaxInt32);
+        public static readonly Mutex ToProduce = new Mutex();
+        public static readonly Mutex ToConsume = new Mutex();
+        public static readonly Mutex RandomAccess = new Mutex();
+    }
+}
