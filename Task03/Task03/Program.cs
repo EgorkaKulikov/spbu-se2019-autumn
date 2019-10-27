fe9373c5f7a8e3d9f8e3d1b5562b449cd367a6ec
AlekseiPrivalihin <aleksei.privalihin@gmail.com>
2019-10-27 22:23:56 +0300

    missing files added

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The session has begun. Everyone, get in here!");
+            List<Consumer<int>> consumers = new List<Consumer<int>>();
+            List<Producer<int>> producers = new List<Producer<int>>();
+
+            var numProducers = 7;
+            var numConsumers = 7;
+
+            for (int i = 0; i < numProducers; i++)
+            {
+                producers.Add(new Producer<int>(i));
+            }
+            for (int i = 0; i < numConsumers; i++)
+            {
+                consumers.Add(new Consumer<int>(i));
+            }
+
+            while (!Console.KeyAvailable) {}
+            
+            Console.WriteLine("The session is closing.");
+            
+            for (int i = 0; i < numProducers; i++)
+            {
+                producers[i].Cancel();
+            }
+            for (int i = 0; i < numConsumers; i++)
+            {
+                consumers[i].Cancel();
+            }
+
+            Shared<int>.IsEmpty.Release(Shared<int>.MaxInt32);
+        }
+    }
+}
