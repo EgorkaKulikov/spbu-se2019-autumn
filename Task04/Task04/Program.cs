df86c903f8fe5d7e366b7315875982ead13e2b5f
AlekseiPrivalihin <aleksei.privalihin@gmail.com>
2019-10-31 00:34:04 +0300

    Task04_Privalikhin

---
+++ Task04/Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await WebLoader.Load("https://fclmnews.ru");
+        }
+    }
+}
---
+++ Task04/Task04/WebLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    public static class WebLoader
+    {
+        private const string Pattern = @"<a href=""https?:\/\/[^""]+?"">";
+        
+        private static readonly WebClient client = new WebClient();
+        
+        public static async Task Load(string reference)
+        {
+            try
+            {
+                var htmlCode = client.DownloadString(reference);
+                var regex = new Regex(Pattern, RegexOptions.Compiled);
+                var matchedLinks = regex.Matches(htmlCode);
+
+                var subloads = new List<Task>();
+            
+                foreach (Match match in matchedLinks)
+                {
+                    var link = new string((match.Value).Skip(9).SkipLast(2).ToArray());
+                    subloads.Add(Subload(link));
+                }
+
+                await Task.WhenAll(subloads.ToArray());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Error! Failed to open main link");
+            }
+        }
+
+        private static Task Subload(string reference)
+        {
+            try
+            {
+                var sz = client.DownloadStringTaskAsync(reference).Result.Length;
+                Console.WriteLine(reference + " has a size of " + sz + " symbols.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Failed to open link" + reference);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
