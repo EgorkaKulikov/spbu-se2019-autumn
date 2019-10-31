65e5c545fb840eb68a64ad895e6b7a5b37369ce6
azaat <a.r.akhmetyanov@gmail.com>
2019-10-31 13:29:59 +0300

    Task 04 upload

---
+++ Task04/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Task04
+{
+    public static class Constants
+    {
+        public const string Url = "https://github.com";
+    }
+}
---
+++ Task04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            WebAsync webAsyncClient = new WebAsync();
+            await webAsyncClient.GetPageData(Constants.Url, false);
+
+            if (webAsyncClient.PageLoaded)
+            {
+                var pageUrls = webAsyncClient.MatchUrls();
+                var webAsyncTasks = new List<Task>();
+                foreach (var url in pageUrls)
+                {
+                    //Client for sub urls
+                    var subClient = new WebAsync();
+                    webAsyncTasks.Add(subClient.GetPageData(url, true));
+                }
+                await Task.WhenAll(webAsyncTasks);
+            }
+            Console.WriteLine("Finished execution, press any key to exit..");
+            Console.ReadKey();
+        }
+    }
+}
