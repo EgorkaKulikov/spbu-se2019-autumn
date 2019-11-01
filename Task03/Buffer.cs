f6bb246eadd6e7b3c3935147bd78f8c8cdf024cc
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-11-01 17:12:02 +0300

    Create Buffer.cs

---
+++ Task03/Buffer.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task03
+{
+    class Buffer<T> where T: new()
+    {
+        Mutex mutBuffer;
+        Queue<T> queue;
+
+        public Buffer()
+        {
+            mutBuffer = new Mutex();
+            queue = new Queue<T>();
+        }
+
+        public void Push(T elem)
+        {
+            mutBuffer.WaitOne();
+            queue.Enqueue(elem);
+            mutBuffer.ReleaseMutex();
+        }
+
+        public Maybe<T> Pop()
+        {
+            mutBuffer.WaitOne();
+            Maybe<T> res = queue.Count > 0 ? Maybe<T>.Just(queue.Dequeue()) : Maybe<T>.Nothing;
+            mutBuffer.ReleaseMutex();
+            return res;
+        }
+    }
+}
