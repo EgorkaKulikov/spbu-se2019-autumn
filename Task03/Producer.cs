47bf34fa3cb8b3731dd9e7cc4b5dc401e0fb7981
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-11-01 17:12:27 +0300

    Create Producer.cs

---
+++ Task03/Producer.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+
+namespace Task03
+{
+    class Producer<T> where T : new()
+    {
+        string name;
+        Buffer<T> buffer;
+        bool canContinue;
+
+        public Producer(string name, ref Buffer<T> buffer)
+        {
+            this.name = name;
+            this.buffer = buffer;
+            canContinue = true;
+        }
+
+        public void Start()
+        {
+            Thread thread = new Thread(() =>
+            {
+                Console.WriteLine($"{name} begin do some work.");
+                while (canContinue)
+                {
+                    Console.WriteLine($"{name} want to put data in buffer.");
+                    PutData();
+                    Console.WriteLine($"{name} put data in buffer.");
+                }
+                Console.WriteLine($"{name} end do some work.");
+            });
+            thread.Start();
+        }
+
+        public void Stop()
+        {
+            canContinue = false;
+        }
+
+        T SomeWork()
+        {
+            Thread.Sleep(GeneralResources.random.Next(GeneralResources.workTimeProducer.Item1, GeneralResources.workTimeProducer.Item2));
+            return new T();
+        }
+
+        void PutData()
+        {
+            T resultSomeWork = SomeWork();
+            buffer.Push(resultSomeWork);
+        }
+    }
+}
