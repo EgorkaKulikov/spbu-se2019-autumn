8e90ff5d1e19f2a6ca1d4ba4bcde96e8c9043dce
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-11-01 17:01:00 +0300

    Create GeneralResources.cs

---
+++ Task02/GeneralResources.cs
@@ -0,0 +1,10 @@
+namespace Task02
+{
+    public class GeneralResources
+    {
+        public const int INF = 1000000000;
+        public static int[,] graphMatrix;
+        public static (int, int, int)[] graphListEdges;
+        public static int n;
+    }
+}
