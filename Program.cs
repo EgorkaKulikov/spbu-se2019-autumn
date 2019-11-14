58a029e3dad17689d909579e0922fe1febf3413e
bidelya@mail.ru <bidelya@mail.ru>
2019-11-14 22:35:56 +0300

    first try

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    internal class Program
+    {
+        private string webpage;
+        private async Task DownloadPages()
+        {
+            Regex rx = new Regex(@"<a href=\""http(s)://\S+\"">",
+                                 RegexOptions.Singleline| RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(webpage);
+            for (int i = 0; i < matches.Count; i++)
+            {
+                await Task.Run( () =>
+                {
+                    string link = matches[i].Value.Substring(9, matches[i].Value.Length - 11);
+                    WebClient client = new WebClient();
+                    client.DownloadFile(link, "page" + i + ".html");
+                    string fileStr = File.ReadAllText("page" + i + ".html");
+                    Console.WriteLine(link + ": " + fileStr.Length + " symbols");
+                });
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1) Console.WriteLine("You did not provide a link");
+            else
+            {
+                Program pr = new Program();
+                WebClient client = new WebClient ();
+                pr.webpage = client.DownloadString (args[0]);
+                Task task = pr.DownloadPages();
+                task.Wait();
+            }
+        }
+    }
+}
