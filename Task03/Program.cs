188af8a589c8913caef0475187c454f0a59d514e
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-11-16 11:23:37 +0300

    Update Program.cs

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -6,26 +6,26 @@ namespace Task03
     {
         static void Main(string[] args)
         {
-            Consumer<object>[] consumers = new Consumer<object>[GeneralResources.amountConsumers];
             Producer<object>[] producers = new Producer<object>[GeneralResources.amountProducers];
+            Consumer<object>[] consumers = new Consumer<object>[GeneralResources.amountConsumers];
 
-            for (int i = 0; i < GeneralResources.amountConsumers; i++)
-            {
-                consumers[i] = new Consumer<object>($"Consumer {i + 1}", ref GeneralResources.mainBuffer);
-                consumers[i].Start();
-            }
             for (int i = 0; i < GeneralResources.amountProducers; i++)
             {
                 producers[i] = new Producer<object>($"Producer {i + 1}", ref GeneralResources.mainBuffer);
                 producers[i].Start();
             }
+            for (int i = 0; i < GeneralResources.amountConsumers; i++)
+            {
+                consumers[i] = new Consumer<object>($"Consumer {i + 1}", ref GeneralResources.mainBuffer);
+                consumers[i].Start();
+            }
 
             Console.ReadKey();
 
-            for (int i = 0; i < GeneralResources.amountConsumers; i++)
-                consumers[i].Stop();
             for (int i = 0; i < GeneralResources.amountProducers; i++)
                 producers[i].Stop();
+            for (int i = 0; i < GeneralResources.amountConsumers; i++)
+                consumers[i].Stop();
         }
     }
 }
