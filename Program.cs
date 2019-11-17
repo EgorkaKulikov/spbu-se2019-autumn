646a96519c912d30919768313f3ce9717198ed12
Gleb.Mirzazyanov <gleb.mirzazyanov@gmail.com>
2019-11-17 21:12:41 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,47 +1,15 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Taks04
 {
-    internal class Program
+    internal static class Program
     {
-        private static readonly string url = "https://belkasoft.com";
-        static readonly WebClient client = new WebClient();
-        static readonly Regex Regex = new Regex(@"<a href=.http\S*");
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Solve();
-        }
-
-        static void Solve()
-        {
-            var strPageCode = client.DownloadString(url);
-            var links = Regex.Matches(strPageCode);
-            var sizeOfRef = links.Count;
-            Console.WriteLine($"Count of valid links: {sizeOfRef}");
-            foreach (var link in links)
-            {
-                OpenRef(link.ToString());
-                System.Threading.Thread.Sleep(1000);
-            }
-        }
-
-        private static async void OpenRef(string url)
-        {
-            url = url.Substring(9, url.Length - 10);
-            var newClient = new WebClient();
-            try
-            {
-                var strPageCode = await newClient.DownloadStringTaskAsync(url);
-                Console.WriteLine("WebPage: {0} - {1}", url, strPageCode.Length);
-            }
-            catch
-            {
-                Console.WriteLine("Incorrect url");
-            }
+            await RefCounter.refCount();
         }
     }
 }
---
+++ refCounter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Taks04
+{
+    internal static class RefCounter
+    {
+        private const string Url = "https://belkasoft.com";
+        private static readonly WebClient Client = new WebClient();
+        private static readonly Regex Regex = new Regex(@"<a href=.http\S*");
+        public static async Task refCount()
+        {
+            var strPageCode = Client.DownloadString(Url);
+            var links = Regex.Matches(strPageCode);
+            var sizeOfRef = links.Count;
+            Console.WriteLine($"Count of valid links: {sizeOfRef}");
+            foreach (var link in links)
+            {
+                await OpenRef(link.ToString());
+            }
+
+            await Task.CompletedTask;
+        }
+        
+        private static async Task OpenRef(string url)
+        {
+            url = url.Substring(9, url.Length - 10);
+            var newClient = new WebClient();
+            try
+            {
+                var strPageCode = await newClient.DownloadStringTaskAsync(url);
+                Console.WriteLine("WebPage: {0} - {1}", url, strPageCode.Length);
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect url");
+            }
+        }
+    }
+}
