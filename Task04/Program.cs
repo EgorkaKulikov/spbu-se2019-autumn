f1c3a09734f800ad525a11b200642a3364ac574b
Lunev Artem <artem.e.lunev2000@gmail.com>
2019-12-01 07:30:55 +0300

    fixies and static deliverance

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,83 +1,14 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Text.RegularExpressions;
 
 namespace Task04
 {
-    class Program
+    public static class Program
     {
-        static string getHTMLcode(string URL)
+        public static void Main(string[] args)
         {
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    Stream receiveStream = response.GetResponseStream();
-                    StreamReader readStream = null;
-
-                    if (response.CharacterSet == null)
-                    {
-                        readStream = new StreamReader(receiveStream);
-                    }
-                    else
-                    {
-                        try
-                        {
-                            readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-                        }
-                        catch (System.ArgumentException)
-                        {
-                            Console.Write("This charset is not provided");
-                            return "This charset is not provided";
-                        }
-
-                    }
-
-                    return readStream.ReadToEnd();
-                }
-            }
-            catch (System.Net.WebException)
-            {
-                Console.Write("Server may be out of order");
-                return "Server may be out of order";
-            }
-            throw new Exception("HttpStatusCode isn't OK");
-        }
-
-        static async void writeSymbolsAsync(List<string> URLs)
-        {
-            foreach (string ur in URLs)
-            {
-                await Task.Run(() => Console.Write(ur + ' ' + getHTMLcode(ur).Length + "\n"));
-            }
-        }
-
-        static void symbols_amount(string URL)
-        {
-            string HTMLcode = getHTMLcode(URL);
-            var matches = Regex.Matches(HTMLcode, "<a href=\"https://.*\">");
-            List<string> URLs = new List<string>();
-
-            foreach (Match match in matches)
-            {
-                URLs.Add(Regex.Match(match.Value, "https://[^\"]*").Value);
-            }
-            writeSymbolsAsync(URLs);
-        }
-
-        static void Main(string[] args)
-        {
-            string srcURL;
-            srcURL = "https://spbu.ru/";
-            symbols_amount(srcURL);
+            var helper = new htmlCodeObserver();
+            string srcURL = "https://spbu.ru/";
+            helper.symbolsAmount(srcURL);
             //end prgramm when get symbol
             Console.ReadKey();
         }
---
+++ Task04/htmlCodeObserver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Task04
+{
+    public class htmlCodeObserver
+    {
+            private string getHTMLcode(string URL)
+            {
+                try
+                {
+                    var request = (HttpWebRequest)WebRequest.Create(URL);
+                    var response = (HttpWebResponse)request.GetResponse();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Stream receiveStream = response.GetResponseStream();
+                        StreamReader readStream = null;
+
+                        if (response.CharacterSet == null)
+                        {
+                            readStream = new StreamReader(receiveStream);
+                        }
+                        else
+                        {
+                            try
+                            {
+                                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                            }
+                            catch (System.ArgumentException)
+                            {
+                                Console.Write("This charset is not provided");
+                                return null;
+                            }
+
+                        }
+
+                        return readStream.ReadToEnd();
+                    }
+                }
+                catch (System.Net.WebException)
+                {
+                    Console.Write("Server may be out of order");
+                    return null;
+                }
+                throw new Exception("HttpStatusCode isn't OK");
+            }
+
+            private async Task writeSymbolsAsync(List<string> URLs)
+            {
+                foreach (string url in URLs)
+                {
+                    await Task.Run(() => Console.Write(url + ' ' + getHTMLcode(url).Length + "\n"));
+                }
+            }
+
+            public async Task symbolsAmount(string url)
+            {
+                string HTMLcode = getHTMLcode(url);
+                var matches = Regex.Matches(HTMLcode, "<a href=\"https://.*\">");
+                List<string> urls = new List<string>();
+
+                foreach (Match match in matches)
+                {
+                    urls.Add(Regex.Match(match.Value, "https://[^\"]*").Value);
+                }
+                await writeSymbolsAsync(urls);
+            }
+    }
+}
