53f054b5fb1f9bbe08b99fd832445629a47c2ccc
Lunev Artem <artem.e.lunev2000@gmail.com>
2019-12-14 23:30:07 +0300

    task02

---
+++ Task02/Prim.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Task02
+{
+    class Prim
+    {
+        public int primAlg(int[,] graph, int size)
+        {
+            bool[] used = new bool[size];
+            int[] minDist = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                used[i] = false;
+                minDist[i] = Int32.MaxValue;
+            }
+            minDist[0] = 0;
+            int weight = 0;
+            for (int i = 0; i < size; i++)
+            {
+                int curr = -1;
+                for (int j = 0; j < size; j++)
+                    if (!used[j] && (curr == -1 || minDist[j] < minDist[curr]))
+                        curr = j;
+                weight += minDist[curr];
+                used[curr] = true;
+                AutoResetEvent done = new AutoResetEvent(false);
+                int numOfDone = 0;
+                for (int j = 0; j < size; j++)
+                {
+                    int tmp = j;
+                    ThreadPool.QueueUserWorkItem(_ =>
+                    {
+                        if (graph[curr, tmp] < minDist[tmp])
+                            minDist[tmp] = graph[curr, tmp];
+                        if (Interlocked.Increment(ref numOfDone) == size)
+                            done.Set();
+                    });
+                }
+                done.WaitOne();
+            }
+            return weight;
+        }
+
+        public void printResult(int[,] graph, int size)
+        {
+            using (var sw = new StreamWriter("Prim.txt"))
+            {
+                var result = primAlg(graph, size);
+                sw.Write(result);
+            }
+        }
+    }
+}
