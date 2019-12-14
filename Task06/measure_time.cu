9c1b3514aaf33bcb639ef6ae996a8485728affe5
azaat <a.r.akhmetyanov@gmail.com>
2019-12-14 21:57:40 +0300

    Average measurements

--- Task06/measure_time.cu
+++ Task06/measure_time.cu
@@ -7,7 +7,8 @@
 #include "bitonic_sort_gpu.cuh"
 
 //Binary logarithm of array length for bitonic sort
-#define MAX_ARRAY_LOG_LEN 25
+#define MAX_ARRAY_LOG_LEN 20
+#define NUM_MEASUREMENTS 10
 
 double get_exec_time(time_t start, time_t stop) {
 	return (double)(stop - start) / CLOCKS_PER_SEC;
@@ -23,6 +24,9 @@ void fill_arr(int *arr, int arr_len) {
 int main()
 {
 	FILE *output = fopen("measurements_output.txt", "w+");
+	//Initializing CUDA context
+	cudaFree(0);
+
 	for (int log_len = 0; log_len < MAX_ARRAY_LOG_LEN; log_len++) {
 		int arr_len = 1 << log_len;
 		size_t arr_size = arr_len * sizeof(int);
@@ -30,21 +34,22 @@ int main()
 		int *temp_arr = (int *)malloc(arr_size);
 		fill_arr(arr, arr_len);
 		clock_t start;
+		double cpu_exec_time = 0;
+		double gpu_exec_time = 0;
 
-		//Initializing CUDA context
-		cudaFree(0);
+		for (int i = 0; i < NUM_MEASUREMENTS; i++) {
+			//CPU version measurement
+			memcpy(temp_arr, arr, arr_size);
+			start = clock();
+			bitonic_sort(temp_arr, log_len);
+			cpu_exec_time += get_exec_time(start, clock()) / NUM_MEASUREMENTS;
 
-		//CPU version measurement
-		memcpy(temp_arr, arr, arr_size);
-		start = clock();
-		bitonic_sort(temp_arr, log_len);
-		double cpu_exec_time = get_exec_time(start, clock());
-
-		//GPU version measurement
-		memcpy(temp_arr, arr, arr_size);
-		start = clock();
-		bitonic_sort_gpu(temp_arr, log_len);
-		double gpu_exec_time = get_exec_time(start, clock());
+			//GPU version measurement
+			memcpy(temp_arr, arr, arr_size);
+			start = clock();
+			bitonic_sort_gpu(temp_arr, log_len);
+			gpu_exec_time += get_exec_time(start, clock()) / NUM_MEASUREMENTS;
+		}
 
 		//Measurements output for gnuplot
 		fprintf(output, "%d %f %f\n", arr_len, gpu_exec_time, cpu_exec_time);
