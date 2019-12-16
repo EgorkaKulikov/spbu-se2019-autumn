d220dd0978d05c0f27e975710f9efbde295daae8
Lunev Artem <artem.e.lunev2000@gmail.com>
2019-12-16 22:19:01 +0300

    task05

---
+++ Task05/CoarseBinaryTree.cs
@@ -0,0 +1,114 @@
+﻿using System.Threading;
+
+namespace Task05
+{
+    public class CoarseBinaryTree
+    {
+        public class Node
+        {
+            public int key;
+            public int value;
+            public Node left = null;
+            public Node right = null;
+            
+            public Node(int key, int value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+        public Node root = null;
+        public Mutex mutex = new Mutex();
+        
+        public bool isBinaryTree(Node node)
+        {
+            if (node == null)
+                return true;
+            if (node.left != null)
+                if (node.left.value > node.value)
+                    return false;
+
+            if (node.right != null)
+                if (node.right.value < node.value)
+                    return false;
+
+            return isBinaryTree(node.left) && isBinaryTree(node.right);
+        }
+        
+        
+
+        public int? find(int key)
+        {
+            mutex.WaitOne();
+            var tempNode = root;
+            while(tempNode != null) {
+                if (tempNode.key == key)
+                {
+                    mutex.ReleaseMutex();
+                    return tempNode.value;
+                }
+                else if (tempNode.key > key)
+                {
+                    tempNode = tempNode.left;
+                }
+                else if (tempNode.key < key)
+                {
+                    tempNode = tempNode.right;
+                }
+            }
+            
+            mutex.ReleaseMutex();
+            return null;
+        }
+
+        public void insert(int key, int value)
+        {
+            mutex.WaitOne();
+            if (root == null)
+            {
+                root = new Node(key, value);
+                mutex.ReleaseMutex();
+                return;
+            }
+
+            var tempNode = root;
+            while (true)
+            {
+                if (tempNode.key > key)
+                {
+                    if (tempNode.left == null)
+                    {
+                        var newNode = new Node(key, value);
+                        tempNode.left = newNode;
+                        mutex.ReleaseMutex();
+                        return;
+                    }
+                    else
+                    {
+                        tempNode = tempNode.left;
+                    }
+                }
+                if (tempNode.key < key)
+                {
+                    if (tempNode.right == null)
+                    {
+                        var newNode = new Node(key, value);
+                        tempNode.right = newNode;
+                        mutex.ReleaseMutex();
+                        return;
+                    }
+                    else
+                    {
+                        tempNode = tempNode.right;
+                    }
+                }
+                if (tempNode.key == key)
+                {
+                    tempNode.value = value;
+                    mutex.ReleaseMutex();
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Task05/FineTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+ 
+namespace Task05
+{
+    public class FineTests
+    {
+        [Test]
+        public void MutexTest()
+        {
+             List<Tuple<int, int>> insertList = new List<Tuple<int, int>>();
+             List<int> findList = new List<int>();
+             var tree = new FineBinaryTree();
+             var tasks = new List<Task>();
+ 
+             for (int i = 0; i < 100; i++)
+             {
+                 var random = new Random();
+                 insertList.Add(new Tuple<int, int>(random.Next(0, 100),random.Next(0, 100)));
+                 findList.Add(random.Next(0, 100));
+             }
+ 
+             foreach (var node in insertList)
+                 tasks.Add(Task.Run(() => tree.insert(node.Item1, node.Item2)));
+ 
+             foreach (var key in findList)
+                 tasks.Add(Task.Run(() => tree.find(key)));
+ 
+             Task.WaitAll(tasks.ToArray());
+             Assert.True(tree.mutexesRealesed(tree.root));
+        }
+ 
+        [Test]
+        public void IsBinaryTreeTest()
+        {
+             List<Tuple<int, int>> insertList = new List<Tuple<int, int>>();
+             List<int> findList = new List<int>();
+             var tree = new FineBinaryTree();
+             var tasks = new List<Task>();
+ 
+             for (int i = 0; i < 100; i++)
+             {
+                 var random = new Random();
+                 insertList.Add(new Tuple<int, int>(random.Next(0, 100),random.Next(0, 100)));
+                 findList.Add(random.Next(0, 100));
+             }
+ 
+             foreach (var node in insertList)
+                 tasks.Add(Task.Run(() => tree.insert(node.Item1, node.Item2)));
+ 
+             foreach (var key in findList)
+                 tasks.Add(Task.Run(() => tree.find(key)));
+ 
+             Task.WaitAll(tasks.ToArray());
+             Assert.True(tree.isBinaryTree(tree.root));
+        }
+    }
+}
